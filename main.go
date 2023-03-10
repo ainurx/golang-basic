c3512c9c0833544c4e8f5bf1897922bf4fdb6431
ainurx <ainurrahmat1@gmail.com>
2023-03-10 18:26:14 +0800

    [master] Add basic func for banking

--- main.go
+++ main.go
@@ -2,10 +2,23 @@ package main
 
 import "fmt"
 
+func loanApplication(balance float32, amount float32) float32 {
+	return balance - amount
+}
+
+func deposit(balance float32, amount float32) float32 {
+	return balance + amount
+}
+
 func main() {
 	var name string = "ainur"
 	age := 24
+	const minBalance = 100000
+	var balance float32 = 150000000
 
 	fmt.Println("Hello", name)
 	fmt.Println("My age is", age)
+
+	fmt.Println("Min balance is Rp.", minBalance)
+	fmt.Printf(`Your balance is Rp.%f\n`, balance)
 }
