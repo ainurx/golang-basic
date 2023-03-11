d28f3aa79ed649bbe4aea856e9ad47a5ae9aa4f9
ainurx <ainurrahmat1@gmail.com>
2023-03-11 20:30:38 +0800

    [master] import math package

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
-func loanApplication(balance float32, amount float32) float32 {
+func loanApplication(balance float64, amount float64) float64 {
 	return balance - amount
 }
 
-func deposit(balance float32, amount float32) float32 {
+func deposit(balance float64, amount float64) float64 {
 	return balance + amount
 }
 
@@ -14,11 +17,15 @@ func main() {
 	var name string = "ainur"
 	age := 24
 	const minBalance = 100000
-	var balance float32 = 150000000
+	var balance float64 = 150000000
 
 	fmt.Println("Hello", name)
 	fmt.Println("My age is", age)
 
 	fmt.Println("Min balance is Rp.", minBalance)
-	fmt.Printf(`Your balance is Rp.%f\n`, balance)
+	fmt.Printf("Your balance is Rp.%f\n", balance)
+
+	var updatedBalance = loanApplication(balance, 30000.0)
+	fmt.Printf("balance after loan Rp.%f\n", updatedBalance)
+	fmt.Printf("balance after loan Rp.", math.Round(updatedBalance))
 }
