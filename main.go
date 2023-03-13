ee24541ce142917d840a92041f4089d2de34dda9
ainurx <ainurrahmat1@gmail.com>
2023-03-13 22:32:30 +0800

    [master] Bug string input compare resolved

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 )
 
 func loanApplication(balance float64, amount float64) float64 {
@@ -28,13 +29,25 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Println("Halo ", name)
+	fmt.Print("PIN: ")
+	pin, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	fmt.Println("My age is", age)
+	if strings.TrimRight(pin, "\n") == "123" {
+		fmt.Println("Halo ", name)
 
-	fmt.Println("Min balance is Rp.", minBalance)
-	fmt.Printf("Your balance is Rp.%f\n", balance)
+		fmt.Println("My age is", age)
 
-	var updatedBalance = loanApplication(balance, 30000.0)
-	fmt.Printf("balance after loan Rp.%f\n", updatedBalance)
+		fmt.Println("Min balance is Rp.", minBalance)
+		fmt.Printf("Your balance is Rp.%f\n", balance)
+
+		var updatedBalance = loanApplication(balance, 30000.0)
+		fmt.Printf("balance after loan Rp.%f\n", updatedBalance)
+	} else {
+		fmt.Println(pin)
+		fmt.Println("PIN anda salah")
+	}
+
 }
